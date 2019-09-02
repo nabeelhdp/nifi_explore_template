a3378b7268bf18feb094afb031d1bf330911b705
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2019-09-02 15:09:53 +0800

    List processors with more than N concurrent tasks scheduled

---
+++ list_concurrent_processors.py
@@ -0,0 +1,29 @@
+
+import xml.etree.ElementTree as ET
+from __future__ import print_function
+
+tree = ET.parse('rootpg.xml')
+root = tree.getroot()
+root = tree.getroot()
+
+for child in root:
+  if child.tag=='snippet':
+    for elements in child:
+      if elements.tag=='processGroups':
+        for process_groups in elements:
+          pg_name=elements.findall('./name')[0].text
+          pg_id=elements.findall('./id')[0].text
+          #if process_groups.tag=='contents':
+          if 1==1:
+            title_flag=1
+            for pg_elements in process_groups:
+              if pg_elements.tag=='processors':
+                concurrentlySchedulableTaskCount="Undefined"
+                processor_name=pg_elements.findall('./name')
+                task_count=pg_elements.findall('./config/concurrentlySchedulableTaskCount')
+                if int(task_count[0].text) > 1:
+                  if title_flag==1:
+                    print("\nName: {} [ ID: {} ]".format(pg_name,pg_id))
+                    title_flag=0
+                  print("    {} : {}".format(processor_name[0].text,task_count[0].text))
+                  
