066157d84f6441e8f90bfc794e79620e7540edf0
nabeelhdp <32059729+nabeelhdp@users.noreply.github.com>
2019-09-02 15:12:15 +0800

    Update list_concurrent_processors.py

--- list_concurrent_processors.py
+++ list_concurrent_processors.py
@@ -6,6 +6,8 @@ tree = ET.parse('rootpg.xml')
 root = tree.getroot()
 root = tree.getroot()
 
+THRESHOLD=1
+
 for child in root:
   if child.tag=='snippet':
     for elements in child:
@@ -13,17 +15,15 @@ for child in root:
         for process_groups in elements:
           pg_name=elements.findall('./name')[0].text
           pg_id=elements.findall('./id')[0].text
-          #if process_groups.tag=='contents':
-          if 1==1:
-            title_flag=1
-            for pg_elements in process_groups:
-              if pg_elements.tag=='processors':
-                concurrentlySchedulableTaskCount="Undefined"
-                processor_name=pg_elements.findall('./name')
-                task_count=pg_elements.findall('./config/concurrentlySchedulableTaskCount')
-                if int(task_count[0].text) > 1:
-                  if title_flag==1:
-                    print("\nName: {} [ ID: {} ]".format(pg_name,pg_id))
-                    title_flag=0
-                  print("    {} : {}".format(processor_name[0].text,task_count[0].text))
+          title_flag=1
+          for pg_elements in process_groups:
+            if pg_elements.tag=='processors':
+              concurrentlySchedulableTaskCount="Undefined"
+              processor_name=pg_elements.findall('./name')
+              task_count=pg_elements.findall('./config/concurrentlySchedulableTaskCount')
+              if int(task_count[0].text) > THRESHOLD:
+                if title_flag==1:
+                  print("\nName: {} [ ID: {} ]".format(pg_name,pg_id))
+                  title_flag=0
+                print("    {} : {}".format(processor_name[0].text,task_count[0].text))
                   
